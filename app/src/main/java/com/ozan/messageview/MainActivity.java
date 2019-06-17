140ece4be49c07b5feb167c142fce8138423de7c
09370458 <ozan_urkan@kocsistem.com.tr>
2019-06-17 17:37:56 +0300

    Build version düşürüldü

--- app/src/main/java/com/ozan/messageview/MainActivity.java
+++ app/src/main/java/com/ozan/messageview/MainActivity.java
@@ -1,8 +1,8 @@
 package com.ozan.messageview;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
